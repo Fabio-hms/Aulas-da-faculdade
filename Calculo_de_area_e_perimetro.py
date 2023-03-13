ce0d2a369d89ba3af867f6f5f13fecf65df24df6
Fabio-hms <127763079+Fabio-hms@users.noreply.github.com>
2023-03-13 11:57:20 -0300

    Add files via upload

---
+++ Calculo_de_area_e_perimetro.py
@@ -0,0 +1,8 @@
+#Calculo de area e perimetro de terreno
+#print('Digite o comprimento do terreno:')
+#C=float(input())
+#print('Digite a largura do terreno:')
+#L=float(input())
+#P = 2*(L+C)
+#A = C*L
+#print(f'Area = {A}, Perimetro = {P}')
