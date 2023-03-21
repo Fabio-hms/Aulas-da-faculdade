a3ced995af760ca107e43d2fe5586c612aa88610
Fabio-hms <127763079+Fabio-hms@users.noreply.github.com>
2023-03-21 08:14:41 -0300

    Add files via upload

---
+++ Valor de salario.py
@@ -0,0 +1,7 @@
+print(' Digite o valor de seu salário:')
+S = float(input())
+print('Digite o valor das vendas realizadas:')
+V = float(input())
+VC = V*0.04
+SF = S+VC
+print(f'O valor do seu salario depois da comissão é:{SF}')
